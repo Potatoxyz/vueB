58159a4f716c19d0ab30fc86ee0b7aaa4892c40c
Potato <512006634@qq.com>
2018-08-14 23:42:22 +0800

    part3

--- components/part/part2.vue
+++ components/part/part2.vue
@@ -1,8 +1,41 @@
 <template>
-    <div class="container">
+    <div class="container pt-5 pb-5">
         <div class="row">
-            <h3 class="title-text w-100 text-center p-4">WE SPECIALIZE IN</h3>
-            <p class="w-100 text-center p-2">Lorem Ipsum is simply dummy text <span class="primary-text">type setting industry</span>  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+            <h3 class="title-text w-100 text-center p-3">WE SPECIALIZE IN</h3>
+            <p class="w-100 text-center p-2">Lorem Ipsum is simply dummy text <span class="primary-text">type setting industry</span>
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>
+        </div>
+        <div class="row">
+            <b-card class="text-center  col-md-4 fix-3w">
+                <div class="icon-wrap p-5 mb-2">
+                    <span class="fa fa-plane"></span>
+                </div>
+                <h4 class="card-title title-text">Planing</h4>
+                <p class="card-text">
+                    Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                    content.
+                </p>
+            </b-card>
+            <b-card class="text-center  col-md-4 fix-3w">
+                <div class="icon-wrap p-5 mb-2">
+                    <span class="fa fa-plane"></span>
+                </div>
+                <h4 class="card-title title-text">Planing</h4>
+                <p class="card-text">
+                    Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                    content.
+                </p>
+            </b-card>
+            <b-card class="text-center  col-md-4 fix-3w">
+                <div class="icon-wrap p-5 mb-2">
+                    <span class="fa fa-plane"></span>
+                </div>
+                <h4 class="card-title title-text">Planing</h4>
+                <p class="card-text">
+                    Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+                    content.
+                </p>
+            </b-card>
         </div>
     </div>
 </template>
@@ -13,4 +46,26 @@
 </script>
 <style lang="scss">
     @import "../public";
+    .fix-3w{
+        margin: 1.5% 0;
+    }
+    @media  (min-width:768px) {
+        .fix-3w{
+            margin: 1.5%;
+            flex-basis: 30.3333%;
+        }
+    }
+
+    .card .icon-wrap {
+        background: $primary;
+        display: inline-block;
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+        text-align: center;
+        span {
+            color: white;
+            font-size: 30px;
+        }
+    }
 </style>
---
+++ components/part/part3.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="part3">
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h3 class="text-white">Happy & excited about company</h3>
+                <h5 class="text-black-50">Happy & excited about company</h5>
+                <p class="text-white-50">
+                    A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of existence.
+                </p>
+                <div class="quote blockquote">
+                    <span class="fa fa-quote-left mr-4 ml-4"></span>
+                    <p>There are two types of people who will tell you that you cannot make a difference in this world: those who are afraid to try and those who are afraid you will succeed. Ray Goforth</p>
+                </div>
+                <p class="ptext">Over the past 11 years we've worked with some amazing small businesses, non-profits, academic institutions and even Fortune 100 companies.</p>
+            </div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "part3"
+    }
+</script>
+
+<style lang="scss">
+    @import "../public.scss";
+    .part3{
+        background: $primary;
+        .quote{
+            display: flex;
+            border-left: 3px solid black;
+            .fa{
+                font-size: 30px;
+            }
+            .ptext{
+                font-size: 1rem;
+                color:darken(white,70%);
+            }
+        }
+    }
+</style>
